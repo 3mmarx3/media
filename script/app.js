36a3e14a7678f09437eaae0cf26490e32b4d6714
Eng-Àmaàr Àh Med <114263245+3mmarx3@users.noreply.github.com>
2024-08-06 08:21:08 +0300

    Update app.js

--- script/app.js
+++ script/app.js
@@ -3,15 +3,31 @@ var toggleSwitch = document.querySelector(".toggle-button");
 function switchTheme(e) {
   if (e.target.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
-    /*If required*/
     document.body.classList.add("dark-mode");
     document.body.classList.remove("light-mode");
+    localStorage.setItem("theme", "dark"); // Save to local storage
   } else {
     document.documentElement.setAttribute("data-theme", "light");
-    /*If required*/
     document.body.classList.add("light-mode");
     document.body.classList.remove("dark-mode");
+    localStorage.setItem("theme", "light"); // Save to local storage
   }
 }
 
+// Add event listener
 toggleSwitch.addEventListener("change", switchTheme, false);
+
+// Check local storage for theme
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme) {
+  document.documentElement.setAttribute("data-theme", currentTheme);
+  if (currentTheme === "dark") {
+    document.body.classList.add("dark-mode");
+    document.body.classList.remove("light-mode");
+    toggleSwitch.checked = true; // Update the toggle switch state
+  } else {
+    document.body.classList.add("light-mode");
+    document.body.classList.remove("dark-mode");
+    toggleSwitch.checked = false; // Update the toggle switch state
+  }
+}

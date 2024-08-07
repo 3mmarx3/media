f9049c5a43dd5ad1410c9a553de119ba6953deed
Ammar Ahmed <ammarahmed@outlook.com>
2024-08-07 18:08:38 +0300

    x

--- script/app.js
+++ script/app.js
@@ -1,33 +1,29 @@
-var toggleSwitch = document.querySelector(".toggle-button");
+let titles = document.querySelectorAll(".chat-list-header");
 
-function switchTheme(e) {
-  if (e.target.checked) {
-    document.documentElement.setAttribute("data-theme", "dark");
-    document.body.classList.add("dark-mode");
-    document.body.classList.remove("light-mode");
-    localStorage.setItem("theme", "dark"); // Save to local storage
-  } else {
-    document.documentElement.setAttribute("data-theme", "light");
-    document.body.classList.add("light-mode");
-    document.body.classList.remove("dark-mode");
-    localStorage.setItem("theme", "light"); // Save to local storage
-  }
+for (let i = 0; i < titles.length; i++) {
+  titles[i].addEventListener("click", function () {
+    let result = this.nextElementSibling;
+    let isActive = result.classList.contains("active");
+    this.classList.toggle("active");
+    result.classList.toggle("active");
+
+    let totalHeight = 0;
+    if (!isActive) {
+      for (let j = 0; j < result.children.length; j++) {
+        totalHeight += result.children[j].scrollHeight + 40;
+      }
+    }
+    result.style.maxHeight = totalHeight + "px";
+  });
 }
 
-// Add event listener
-toggleSwitch.addEventListener("change", switchTheme, false);
+const themeColors = document.querySelectorAll(".theme-color");
 
-// Check local storage for theme
-const currentTheme = localStorage.getItem("theme");
-if (currentTheme) {
-  document.documentElement.setAttribute("data-theme", currentTheme);
-  if (currentTheme === "dark") {
-    document.body.classList.add("dark-mode");
-    document.body.classList.remove("light-mode");
-    toggleSwitch.checked = true; // Update the toggle switch state
-  } else {
-    document.body.classList.add("light-mode");
-    document.body.classList.remove("dark-mode");
-    toggleSwitch.checked = false; // Update the toggle switch state
-  }
-}
+themeColors.forEach((themeColor) => {
+  themeColor.addEventListener("click", () => {
+    themeColors.forEach((c) => c.classList.remove("active"));
+    const theme = themeColor.getAttribute("data-color");
+    document.body.setAttribute("data-theme", theme);
+    themeColor.classList.add("active");
+  });
+});

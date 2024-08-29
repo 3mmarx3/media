86a393c937801781b5aebccd8c6cfb13f369941a
Ammar Ahmed <ammarahmed@outlook.com>
2024-08-30 01:13:23 +0300

    ED

--- script/app.js
+++ script/app.js
@@ -1,45 +1,3 @@
-const currentDay = new Date().getDay();
-
-const days = [
-  "sunday",
-  "monday",
-  "tuesday",
-  "wednesday",
-  "thursday",
-  "friday",
-  "saturday",
-];
-
-document.getElementById(days[currentDay]).classList.add("highlight");
-
-function getCurrentTime() {
-  const now = new Date();
-  let hours = now.getHours();
-  const minutes = now.getMinutes().toString().padStart(2, "0");
-  const ampm = hours >= 12 ? "PM" : "AM";
-  hours = hours % 12;
-  hours = hours ? hours : 12;
-  hours = hours.toString().padStart(2, "0");
-  return `${hours}:${minutes} ${ampm}`;
-}
-
-const timeElement = document.querySelector(".info-text-upper");
-timeElement.textContent = getCurrentTime();
-
-setInterval(() => {
-  timeElement.textContent = getCurrentTime();
-}, 1000);
-
-function getCurrentDate() {
-  const now = new Date();
-  const day = now.getDate().toString().padStart(2, "0");
-  const month = (now.getMonth() + 1).toString().padStart(2, "0");
-  return `${day}/${month}`;
-}
-
-const dateElement = document.querySelector(".date");
-dateElement.textContent = getCurrentDate();
-
 //
 
 var toggleSwitch = document.querySelector(".toggle-button");
@@ -160,5 +118,5 @@ left_ptn.addEventListener("click", function () {
 });
 
 left_pt.addEventListener("click", function () {
-  left_S.style.left = "100vh";
+  left_S.style.left = "1000vh";
 });
